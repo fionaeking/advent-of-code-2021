0a9b195534a320f1d0d8eb957357fa785aa0a29a
Fiona King <fiona.king@metaswitch.com>
2022-01-06 22:33:18 +0000

    Tidy up

--- Day1.ts
+++ Day1.ts
@@ -5,22 +5,7 @@ let inputValues: Array<number> = fs
   .readFileSync("input.txt", "utf8")
   .toString()
   .split("\n")
-  .map(Number);
+  .map(Number); 
 
-// Part 1
-var part1Increases = 0;
-inputValues.forEach(
-  (_rowValue, index) => {
-    if (inputValues[index+1] > inputValues[index]) {part1Increases++;}
-  }
-);
-console.log("Part 1 answer:", part1Increases);
-
-// Part 2
-var part2Increases = 0;
-inputValues.forEach(
-  (_rowValue, index) => {
-    if (inputValues.slice(index+1, index+4).reduce((sum, current) => sum + current, 0) > (inputValues.slice(index, index+3).reduce((sum, current) => sum + current, 0))) {part2Increases++;}
-  }
-);
-console.log("Part 2 answer:", part2Increases);
+console.log("Part 1 answer:", inputValues.filter((_rowValue, i) => inputValues[i+1] > inputValues[i]).length);
+console.log("Part 2 answer:", inputValues.filter((_rowValue, i) => inputValues[i+3] > inputValues[i]).length);
