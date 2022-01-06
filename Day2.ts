85cfccdff6717a1925d0c9cb7631d1677d976d79
Fiona King <fiona.king@metaswitch.com>
2022-01-06 22:50:25 +0000

    Add Day2

---
+++ Day2.ts
@@ -0,0 +1,38 @@
+import * as fs from "fs";
+
+// Read in input
+let inputValues: Array<string> = fs
+  .readFileSync("input.txt", "utf8")
+  .toString()
+  .split("\n");
+
+  var x = 0;
+  var y = 0;
+  var aim = 0;
+
+// TODO v quick and not efficient - tidy up!
+
+// Part 1
+inputValues.forEach( instruction => {
+  var array = instruction.split(" ");
+  var value = Number(array[1]);
+  switch (array[0])
+  {
+    case 'forward':
+      x += value;
+      y += aim * value;
+      break;
+    case 'down':
+      // y += value;
+      aim += value;
+      break;
+    case 'up':
+      // y -= value;
+      aim -= value;
+      break;
+    default:
+      break;
+  }
+})
+console.log(x * y);
+
