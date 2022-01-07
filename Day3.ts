dfc6d43f26b363ead6e9cd7eb762a74733a2d69b
Fiona King <fiona.king@metaswitch.com>
2022-01-07 21:52:12 +0000

    Add Day3

--- Day3.ts
+++ Day3.ts
@@ -6,6 +6,7 @@ let inputValues: Array<string> = fs
   .toString()
   .split("\n");
 
+// Part 1
 function binary_to_decimal(decimal: number) : number {
   return Math.pow(2, decimal)
 }
@@ -22,11 +23,30 @@ for (var i = 0; i < numberLength; i++) {
       return x + y;
     }, 0);
 
-  if (sum * 2 > inputValues.length) {
+  if (sum * 2 >= inputValues.length) {
     gammaRate += binary_to_decimal(numberLength - 1 - i);
   } else {
     epsilonRate += binary_to_decimal(numberLength - 1 - i);
   }
 }
-
 console.log("Part 1 answer:", gammaRate * epsilonRate);
+
+
+function apply_bit_criteria(inputArray: string[], i: number, most_common_value: boolean)
+{
+  var sumO2 = inputArray
+  .map(function (x) {
+    return Number(x[i]);
+  })
+  .reduce(function (x, y) {
+    return x + y;
+  }, 0);
+  return (sumO2 * 2 >= inputArray.length) ? inputArray.filter((value) => value[i] == (most_common_value ? '1' : '0')) : inputArray.filter((value) => value[i] == (most_common_value ? '0' : '1'));
+}
+
+var clonedInputValues  = [...inputValues];
+for (var i = 0; i < numberLength; i++) {
+  if (inputValues.length > 1) inputValues = apply_bit_criteria(inputValues, i, true);
+  if (clonedInputValues.length > 1) clonedInputValues = apply_bit_criteria(clonedInputValues, i, false);
+}
+console.log("Part 2 answer:", parseInt(inputValues[0], 2) * parseInt(clonedInputValues[0], 2));
