23f89dab30c4275122e8eb2d7954e4be041b109e
Fiona King <fiona.king@metaswitch.com>
2022-01-12 12:25:50 +0000

    Add Day 7 part 1

---
+++ Day7.ts
@@ -0,0 +1,23 @@
+import * as fs from "fs";
+
+function median(inputArray: number[]){
+  inputArray.sort(function(a: number,b: number){return a-b;});
+  var half = Math.floor(inputArray.length / 2);
+  return (inputArray.length % 2) ? inputArray[half] : (inputArray[half - 1] + inputArray[half]) / 2;
+}
+
+// Read in input
+let inputValues: Array<number> = fs
+  .readFileSync("input.txt", "utf8")
+  .toString()
+  .split(",")
+  .map(Number);
+
+var medianNum = median(inputValues);
+
+var sum = 0;
+inputValues.forEach(x => {
+  sum += Math.abs(x-medianNum);
+})
+
+console.log("Part 1 answer: ", sum);
---
+++ README.md
@@ -0,0 +1,5 @@
+Run each file by 
+
+ts-node <filename>
+
+e.g ts-node Day1.ts
