1ab2dcdb918f982ccc9c0915ee33dec01f3d98ba
Fiona King <fiona.king@metaswitch.com>
2022-01-13 14:26:17 +0000

    Add Day 8 part 1

---
+++ Day8.ts
@@ -0,0 +1,15 @@
+import * as fs from "fs";
+
+// Read in input
+let inputValues: Array<string> = fs
+  .readFileSync("input.txt", "utf8")
+  .toString()
+  .split("\n");
+
+var sum = 0;
+inputValues.forEach( row => {
+  row.split('|')[1].trim().split(/\s+/).forEach(digitOut => 
+    { sum += +[2, 3, 4, 7].includes(digitOut.length); })
+});
+
+console.log("Part 1 answer:", sum);
